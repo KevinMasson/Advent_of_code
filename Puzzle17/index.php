1ccab33d5d22b1f9d265529e3e27b9423e19a28a
Kevin MASSON <Kevin.MASSON@armatis.corp>
2020-12-09 11:27:08 +0100

    Jour 9

---
+++ Puzzle17/index.php
@@ -0,0 +1,38 @@
+<?php
+
+$data = fopen('data.txt', 'r');
+$DataArray=array();
+while(!feof($data)){
+	$Line = fgets($data);
+	array_push($DataArray, trim($Line));
+}
+
+$StartI=0;
+$Range=25;
+$Valid=true;
+$RangeInProgress=array($StartI,$Range-1);
+$Cpt=$Range;
+
+
+function isValid($Value,$Range,$DataArray){
+	$Valid=false;
+	for($Cpt1=$Range[0];$Cpt1<=$Range[1];$Cpt1++){
+		for($Cpt2=$Range[0];$Cpt2<=$Range[1];$Cpt2++){
+			//echo $Cpt1." ".$Cpt2." ".$DataArray[$Cpt1]." ".$DataArray[$Cpt2]." ".$Value."<br>";
+			if($Cpt1!=$Cpt2 && $DataArray[$Cpt1]+$DataArray[$Cpt2]==$Value){
+				$Valid=true;
+			}
+		}	
+	}
+	return $Valid;
+}
+
+while($Valid){
+	if(!isValid($DataArray[$Cpt],$RangeInProgress,$DataArray)){break;}
+	$Cpt++;
+	$RangeInProgress[0]=$RangeInProgress[0]+1;
+	$RangeInProgress[1]=$RangeInProgress[1]+1;
+}
+echo $DataArray[$Cpt];
+
+?>
