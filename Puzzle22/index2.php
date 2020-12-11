7c03254cd5fb1c017436278f1bfa38793beed2a4
Kevin MASSON <Kevin.MASSON@armatis.corp>
2020-12-11 16:52:13 +0100

    D11 bis

---
+++ Puzzle22/index2.php
@@ -0,0 +1,70 @@
+<?php
+$TotalToSrc=2020;
+$NbNumber =3;
+$data = fopen('data.txt', 'r');
+$DataArray=array();
+while(!feof($data)){
+	$Line = fgets($data);
+	array_push($DataArray,str_split(trim($Line)));
+}
+
+
+function scroll($DataArray,$x,$y,$J,$Car,$CarBloc=''){
+	$Nbadj=0;
+	if(($x+$J[0])>=0 &&($x+$J[0]) <count($DataArray)){
+		if(($y+$J[1])>=0 &&($y+$J[1]) <count($DataArray[($x+$J[0])])){
+			switch($DataArray[($x+$J[0])][($y+$J[1])]){
+				case 'L' :
+					break;
+				case '.' :
+					$Nbadj+=scroll($DataArray,($x+$J[0]),($y+$J[1]),$J,$Car,$CarBloc='');
+					break;
+				case '#' :
+					$Nbadj++;
+					break;
+			}
+		}
+	}
+	return $Nbadj;
+}
+
+
+function CheckAdjVisible($DataArray,$x,$y,$Car,$CarBloc=''){
+	$Nbadj=0;
+	$Jmp=array(array(-1,-1),array(-1,0),array(-1,1),array(0,-1),array(0,1),array(1,-1),array(1,0),array(1,1));
+	foreach($Jmp as $J){
+		$Nbadj+=scroll($DataArray,$x,$y,$J,$Car,$CarBloc='');
+	}
+	return $Nbadj;
+}
+
+
+$WorkTab=$DataArray;
+$PreviousWorkTab=array();
+while ($PreviousWorkTab!=$WorkTab){
+	$PreviousWorkTab=$WorkTab;
+	for($i=0;$i<count($WorkTab);$i++){
+		for($j=0;$j<count($WorkTab[$i]);$j++){
+			switch($WorkTab[$i][$j]){
+				case 'L' :
+					$WorkTab[$i][$j]=(CheckAdjVisible($PreviousWorkTab,$i,$j,"#","L")==0 ? '#' :'L');
+					break;
+				case '.' :
+					break;
+				case '#' :
+					$WorkTab[$i][$j]=(CheckAdjVisible($PreviousWorkTab,$i,$j,"#","L")<5 ? '#' :'L');
+					break;
+			}
+		}
+	}
+}
+$NbOcc=0;
+for($i=0;$i<count($WorkTab);$i++){
+	for($j=0;$j<count($WorkTab[$i]);$j++){
+		echo $WorkTab[$i][$j];
+		$NbOcc+=$WorkTab[$i][$j]=="#"? 1 : 0;	
+	}
+	echo "<br>";
+}
+echo "NbOcc : ".$NbOcc;
+?>
