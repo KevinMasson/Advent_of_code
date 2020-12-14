f752e306c78f9cff728c1c052d9539c0e9277afc
Kevin MASSON <Kevin.MASSON@armatis.corp>
2020-12-14 13:15:13 +0100

    D14-2

---
+++ Puzzle25/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$data = fopen('data.txt', 'r');
+$DataArray=array();
+//while(!feof($data)){
+	//$EarlierTime = fgets($data);
+	var_dump(fgets($data));
+	//array_push($DataArray, trim($Line));
+//}
+/*
+$EarlierTime=$DataArray[0];
+$Buses=explode($DataArray[1],",");
+*/
+
+
+
+
+?>
